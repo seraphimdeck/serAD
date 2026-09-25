1efb551c20220049b5cbd5bd078e4efcb054403c
seraphimdeck <255605574+seraphimdeck@users.noreply.github.com>
2026-09-25 22:14:09 +0000

    local scope safety checks

---
+++ pkg/gatekeeper/ttl.go
@@ -0,0 +1,37 @@
+package gatekeeper
+
+import (
+	"fmt"
+	"net"
+	"syscall"
+	"time"
+)
+
+func (g *Gatekeeper) CheckTTL() error {
+	address := fmt.Sprintf("%s:%d", g.TargetIP, g.Port)
+
+	var controlErr error
+
+	dialer := net.Dialer{
+		Timeout: 2 * time.Second,
+		Control: func(network, address string, c syscall.RawConn) error {
+			return c.Control(func(fd uintptr) {
+				err := syscall.SetsockoptInt(int(fd), syscall.IPPROTO_IP, syscall.IP_TTL, 1)
+				if err != nil {
+					controlErr = fmt.Errorf("gagal mengkonfigurasi IP_TTL=1: %w", err)
+				}
+			})
+		},
+	}
+
+	conn, err := dialer.Dial("tcp", address)
+	if err != nil {
+		if controlErr != nil {
+			return controlErr
+		}
+		return fmt.Errorf("probe TTL=1 gagal ke %s: target berada di luar jaringan lokal (terdeteksi router hop)", address)
+	}
+	defer conn.Close()
+
+	return nil
+}
